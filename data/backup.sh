398d8adf47628ce0c787e40ff20c31ba7f08bcf0
Steve Zelek <11831150+smzelek@users.noreply.github.com>
2022-10-22 07:46:42 -0700

    fix: handle noisy ec2 logs taking up all 8gb of reserved space lmao

--- data/backup.sh
+++ data/backup.sh
@@ -11,7 +11,13 @@ echo "Deleting old backups." | ts
 echo "Deleting old backups complete." | ts
 
 echo "Clearing noisy tmod log." | ts
-du -hcs ~/tmodserver/tModLoader-Logs/server.log | ts
-> ~/tmodserver/tModLoader-Logs/server.log
-du -hcs ~/tmodserver/tModLoader-Logs/server.log | ts
+du -hcs ~/tmodserver/tModLoader-Logs/* | ts
+find ~/tmodserver/tModLoader-Logs/server.* -type f -print0 | xargs -0 truncate -s0
+du -hcs ~/tmodserver/tModLoader-Logs/* | ts
 echo "Done clearing noisy tmod log." | ts
+
+echo "Clearing noisy journal log." | ts
+du -hcs /var/log/journal/ | ts
+journalctl --vacuum-size=50M | ts
+du -hcs /var/log/journal/ | ts
+echo "Done clearing noisy journal log." | ts
